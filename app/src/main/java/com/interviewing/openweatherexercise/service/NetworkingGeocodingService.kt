48c595491b5b20aa6917a32244ec7e49cbd5a84e
volmarias <volmarias@volmari.as>
2024-09-18 13:30:14 -0400

    Geocoding LRU cache

--- app/src/main/java/com/interviewing/openweatherexercise/service/NetworkingGeocodingService.kt
+++ app/src/main/java/com/interviewing/openweatherexercise/service/NetworkingGeocodingService.kt
@@ -1,5 +1,6 @@
 package com.interviewing.openweatherexercise.service
 
+import android.util.LruCache
 import com.interviewing.openweatherexercise.api.GeocodingEndpoint
 import com.interviewing.openweatherexercise.service.GeocodingService.GeocodedLocation
 import kotlinx.coroutines.Dispatchers
@@ -7,13 +8,22 @@ import kotlinx.coroutines.withContext
 import okio.IOException
 import timber.log.Timber
 
-class NetworkingGeocodingService(private val geocodingEndpoint: GeocodingEndpoint) : GeocodingService {
+class NetworkingGeocodingService(private val geocodingEndpoint: GeocodingEndpoint) :
+    GeocodingService {
+
+    // TODO: In real code, this would be disk access based.
+    private val nameLruCache: LruCache<String, List<GeocodedLocation>> = LruCache(32)
+    private val coordsLruCache: LruCache<Pair<Double, Double>, List<GeocodedLocation>> =
+        LruCache(32)
+
     override suspend fun fetchCoordsForName(query: String, limit: Int): List<GeocodedLocation> {
         return withContext(Dispatchers.IO) {
             Timber.i("Requesting coordinate for $query")
-            with (geocodingEndpoint.directQuery(query, limit = limit).execute()) {
+            nameLruCache.get(query) ?: with(
+                geocodingEndpoint.directQuery(query, limit = limit).execute()
+            ) {
                 if (isSuccessful && body() != null) {
-                    body()!!.map { it.toGeocodedLocation() }
+                    body()!!.map { it.toGeocodedLocation() }.also { nameLruCache.put(query, it) }
                 } else {
                     throw IOException("Could not geolocate $query")
                 }
@@ -21,12 +31,23 @@ class NetworkingGeocodingService(private val geocodingEndpoint: GeocodingEndpoin
         }
     }
 
-    override suspend fun fetchNameForCoords(lat: Double, lon: Double, limit: Int): List<GeocodedLocation> {
+    override suspend fun fetchNameForCoords(
+        lat: Double,
+        lon: Double,
+        limit: Int
+    ): List<GeocodedLocation> {
         return withContext(Dispatchers.IO) {
             Timber.i("Requesting name for $lat, $lon")
-            with (geocodingEndpoint.reverseQuery(lat = lat, lon = lon, limit = limit).execute()) {
+            coordsLruCache.get(Pair(lat, lon)) ?: with(
+                geocodingEndpoint.reverseQuery(
+                    lat = lat,
+                    lon = lon,
+                    limit = limit
+                ).execute()
+            ) {
                 if (isSuccessful && body()?.isNotEmpty() == true) {
                     body()!!.map { it.toGeocodedLocation() }
+                        .also { coordsLruCache.put(Pair(lat, lon), it) }
                 } else {
                     throw IOException("Could not find location for $lat, $lon")
                 }
